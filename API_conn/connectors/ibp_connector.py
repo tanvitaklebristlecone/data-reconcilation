a1215c9c72cd2eec3e5a03a465fceb2e6bf3e579
Tanvi Takle <tanvi.takle.bristlecone@gmail.com>
2026-06-15 17:01:26 +0530

    updated changes for ibp conn testing

--- API_conn/connectors/ibp_connector.py
+++ API_conn/connectors/ibp_connector.py
@@ -1,28 +1,47 @@
 from API_conn.connectors.base_connector import SAPConnector
+from API_conn.config.config_loader import load_config
 import pandas as pd
+import requests
+
 
 class IBPDemandConnector(SAPConnector):
 
+    def __init__(self):
+        config = load_config()["ibp"]
+        super().__init__(config)
+
     def fetch(self):
-        """
-        Future:
-        GET /IBP/PLANNING_DATA_API_SRV
-        """
+
+        endpoint = (
+            self.config["base_url"]
+            + self.config["service"]
+        )
+
+        print("\nEndpoint:")
+        print(endpoint)
+
+        response = requests.get(
+            endpoint,
+            auth=(
+                self.config["username"],
+                self.config["password"]
+            ),
+            headers={
+                "Accept": "application/json"
+            },
+            timeout=60,
+            allow_redirects=False
+        )
+
+        print("\nStatus Code:")
+        print(response.status_code)
+
+        print("\nHeaders:")
+        print(response.headers)
+
+        print("\nResponse Preview:")
+        print(response.text[:3000])
+
+        response.raise_for_status()
 
         return pd.DataFrame()
-
-"""
-ibp:
-  base_url: https://<ibp-host>
-  service: /IBP/PLANNING_DATA_API_SRV
-  username: <user>
-  password: <password>
-
-Tenant URL
-Authentication method
-Planning Area
-Key Figure name
-Planning level
-/IBP/PLANNING_DATA_API_SRV endpoint
-
-"""
--- test_ibp.py
+++ test_ibp.py
@@ -2,13 +2,7 @@ from API_conn.connectors.ibp_connector import IBPDemandConnector
 
 connector = IBPDemandConnector()
 
-try:
-    df = connector.fetch()
+df = connector.fetch()
 
-    print("SUCCESS")
-    print(df.head())
-    print(df.shape)
-
-except Exception as e:
-    print("ERROR")
-    print(e)
+print(df.head())
+print(df.shape)
